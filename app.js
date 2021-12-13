91f659703797567ce855ddf694066af21aa4898d
donnie <donfather1000@gmail.com>
2021-12-12 22:02:56 -0500

    add custom string message

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function add(num1, num2, showResult, phrase) {
+    if (showResult) {
+        var sum = num1 + num2;
+        console.log(phrase + sum);
+    }
+    else {
+        return num1 + num2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+
+  if (showResult) {
+    const sum = num1 + num2;
+    console.log(phrase + sum);
+  } else {
+    return num1 + num2;
+  }
+}
+
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
+
+
