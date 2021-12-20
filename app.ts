f88bffb874331db1923699b412fa73674f00b861
donnie <donfather1000@gmail.com>
2021-12-19 19:25:48 -0500

    exercise 2

--- app.ts
+++ app.ts
@@ -1,105 +1,65 @@
 /*
 
-Welcome to:
-
-    ................................................................
-    .                                                              .
-    .     ####################    ####################      E      .
-    .     ####################    ####################      X      .
-    .             ####            ####                      E      .
-    .             ####            ####                      R      .
-    .             ####            ####################      C      .
-    .             ####                            ####      I      .
-    .             ####                            ####      S      .
-    .             ####            ####################      E      .
-    .             ####            ####################      S      .
-    .                                                              .
-    ................................................................
-
-    The goal: Let everyone play with many different TypeScript features
-    and get an overview of TypeScript capabilities and principles.
-
-    Things to cover:
-
-        1. Basic typing.
-        2. Refining types.
-        3. Union types.
-        4. Merged types.
-        5. Generics.
-        6. Type declarations.
-        7. Module augmentation.
-        8. Advanced type mapping.
-
-    Rules and principles:
-
-        1. Avoid using "any" type at all costs.
-        2. Difficulty quickly grows one exercise after another.
-        3. Feel free to send pull-requests if you've come up
-           with improvements!
-        4. Provide feedback to the creator of these exercises.
-        5. Enjoy.
-
-Brief UI guide:
-
-    +--------------------------------------------------------------+
-    | TypeScript exercises                                         |
-    +--------------------------------------------------------------+
-    | Exercises 1·2·3·4...   << Navigate through exercises >>      |
-    +---------------+----------------------------------------------+
-    | Files         | file.ts   << Filename and status >>          |
-    +---------------+----------------------------------------------+
-    | file.ts       | 1  import {x} from 'y';                      |
-    | dir           | 2                                            |
-    |   sub.ts      | 3                                            |
-    |               |                                              |
-    | << Current    |   << Currently selected file code editor >>  |
-    | exercise file |                                              |
-    | structure >>  +----------------------------------------------+
-    |               |                                              |
-    |               |   << Errors to fix in order to proceed >>    |
-    |               |                                              |
-    +---------------+----------------------------------------------+
-
 Intro:
 
-    We are starting a small community of users. For performance
-    reasons we have decided to store all users right in the code.
-    This way we can provide our developers with more
-    user-interaction opportunities. With user-related data, at least.
-    All the GDPR-related issues will be solved some other day.
-    This would be the base for our future experiments during
-    these exercises.
+    All 2 users liked the idea of the community. We should go
+    forward and introduce some order. We are in Germany after all.
+    Let's add a couple of admins.
+
+    Initially we only had users in the in-memory database. After
+    introducing Admins, we need to fix the types so that
+    everything works well together.
 
 Exercise:
 
-    Given the data, define the interface "User" and use it accordingly.
+    Type "Person" is missing, please define it and use
+    it in persons array and logPerson function in order to fix
+    all the TS errors.
 
 */
 
-export type User = unknown;
-
-export const users: unknown[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    }
-];
-
-export function logPerson(user: unknown) {
-    console.log(` - ${user.name}, ${user.age}`);
+interface User {
+  name: string;
+  age: number;
+  occupation: string;
 }
 
-console.log('Users:');
-users.forEach(logPerson);
+interface Admin {
+  name: string;
+  age: number;
+  role: string;
+}
 
+export type Person = unknown;
 
-/* In case if you are stuck:
+export const persons: User[] /* <- Person[] */ = [
+  {
+      name: 'Max Mustermann',
+      age: 25,
+      occupation: 'Chimney sweep'
+  },
+  {
+      name: 'Jane Doe',
+      age: 32,
+      role: 'Administrator'
+  },
+  {
+      name: 'Kate Müller',
+      age: 23,
+      occupation: 'Astronaut'
+  },
+  {
+      name: 'Bruce Willis',
+      age: 64,
+      role: 'World saver'
+  }
+];
 
-// https://www.typescriptlang.org/docs/handbook/interfaces.html#introduction
-*/
+export function logPerson(user: User) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+
+persons.forEach(logPerson);
+
+// In case if you are stuck:
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#union-types
